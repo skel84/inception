09756825cb71d20888fcdaeb2a15562aa0200ea0
Ivan Moore <ivan@teamoptimization.com>
2015-09-12 07:00:25 +0100

    corrected call of ExecTask constructor

--- bootstrap.py
+++ bootstrap.py
@@ -5,22 +5,22 @@ configurator = GoCdConfigurator(HostRestClient('go-server:8153'))
 
 pipeline_name = sys.argv[1]
 
-pipeline = configurator\
-    .ensure_pipeline_group('auto-created')\
+pipeline = configurator \
+    .ensure_pipeline_group('auto-created') \
     .find_pipeline(pipeline_name)
 
 for stage in pipeline.stages()[1:]:
     pipeline.ensure_removal_of_stage(stage.name())
 
-pipeline.ensure_stage('build').ensure_job('build')\
-    .ensure_task(ExecTask('echo build'))
+pipeline.ensure_stage('build').ensure_job('build') \
+    .ensure_task(ExecTask(['echo', 'build']))
 
 environments = open('environments.txt').readlines()
 for environment in environments:
     if environment:
-        pipeline\
-            .ensure_stage('deploy-to-' + environment.strip())\
-            .ensure_job('deploy')\
-            .ensure_task(ExecTask('echo deploy to ' + environment))
+        pipeline \
+            .ensure_stage('deploy-to-' + environment.strip()) \
+            .ensure_job('deploy') \
+            .ensure_task(ExecTask(['echo', 'deploy', 'to', 'environment']))
 
 configurator.save_updated_config()
