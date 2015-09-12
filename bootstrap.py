88f2a6923b6fa4cdd0f7bed10f0e70f257f92900
Ivan Moore <ivan@teamoptimization.com>
2015-09-12 06:49:05 +0100

    made bootstrap a bit more realistic at the cost of being longer and less general

--- bootstrap.py
+++ bootstrap.py
@@ -1,23 +1,26 @@
 import sys
 from gomatic import GoCdConfigurator, HostRestClient, ExecTask
 
-configurator = GoCdConfigurator(HostRestClient("go-server:8153"))
+configurator = GoCdConfigurator(HostRestClient('go-server:8153'))
 
 pipeline_name = sys.argv[1]
 
 pipeline = configurator\
-    .ensure_pipeline_group("auto-created")\
+    .ensure_pipeline_group('auto-created')\
     .find_pipeline(pipeline_name)
 
 for stage in pipeline.stages()[1:]:
     pipeline.ensure_removal_of_stage(stage.name())
 
-commands = open("commands.txt").readlines()
-for command in commands:
-    command_name, thing_to_execute = command.strip().split('=')
-    pipeline\
-        .ensure_stage(command_name)\
-        .ensure_job(command_name)\
-        .ensure_task(ExecTask(thing_to_execute.split(" ")))
+pipeline.ensure_stage('build').ensure_job('build')\
+    .ensure_task(ExecTask('echo build'))
+
+environments = open('environments.txt').readlines()
+for environment in environments:
+    if environment:
+        pipeline\
+            .ensure_stage('deploy-to-' + environment)\
+            .ensure_job('deploy')\
+            .ensure_task(ExecTask('echo deploy to ' + environment))
 
 configurator.save_updated_config()
